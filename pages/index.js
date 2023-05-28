62309d9a3a1e7a3b1c526e690d1deda8d3e0ac0a
whit3rabbit <whit3rabbit@users.noreply.github.com>
2023-05-28 23:38:35 +0000

    working dropdowns

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,14 @@ import ChartComponent from '../components/Chart';
 
 export default function Home() {
   const [data, setData] = useState(null);
-  const [fetchInterval, setFetchInterval] = useState(15) // Renamed from setInterval
-  const [chartType, setChartType] = useState('line')
-  const isFetching = useRef(true) // Changed from useState to useRef
+  const [fetchInterval, setFetchInterval] = useState(15); // Set default interval to 15 seconds
+  const [isFetching, setIsFetching] = useState(true);
+  const [chartType, setChartType] = useState('line');
   const intervalId = useRef(null);
 
   useEffect(() => {
-    const apiKey = Cookies.get('apiKey')
-    const channelId = Cookies.get('channelId')
+    const apiKey = Cookies.get('apiKey');
+    const channelId = Cookies.get('channelId');
 
     const fetchData = async () => {
       try {
@@ -23,40 +23,27 @@ export default function Home() {
       }
     };
 
-    if (apiKey && channelId && isFetching.current) {
-      fetchData()
-      intervalId.current = setInterval(fetchData, fetchInterval * 1000)
+    if (apiKey && channelId && isFetching) {
+      fetchData();
+      intervalId.current = setInterval(fetchData, fetchInterval * 1000);
     }
 
     return () => {
       if (intervalId.current) {
-        clearInterval(intervalId.current)
+        clearInterval(intervalId.current);
       }
-    }
-  }, [fetchInterval])
+    };
+  }, [fetchInterval, isFetching]);
 
   const toggleFetching = () => {
-    isFetching.current = !isFetching.current;
-    if (!isFetching.current && intervalId.current) {
+    if (isFetching && intervalId.current) {
       clearInterval(intervalId.current);
     }
-  }
-
+    setIsFetching(!isFetching);
+  };
+  
   return (
     <div className="flex flex-col justify-center items-center m-4">
-      <select value={fetchInterval} onChange={(e) => setFetchInterval(Number(e.target.value))} className="mb-4">
-        <option value={5}>5 seconds</option>
-        <option value={10}>10 seconds</option>
-        <option value={15}>15 seconds</option>
-        <option value={30}>30 seconds</option>
-        <option value={60}>60 seconds</option>
-      </select>
-
-      <select value={chartType} onChange={(e) => setChartType(e.target.value)} className="mb-4">
-        <option value="line">Line</option>
-        <option value="bar">Bar</option>
-        <option value="pie">Pie</option>
-      </select>
       
       {data ? (
         <div>
@@ -66,9 +53,29 @@ export default function Home() {
                 .map((fieldKey, i) => (
                  <ChartComponent key={i} type={chartType} data={data} fieldKey={fieldKey} />
               ))}
-            <button onClick={toggleFetching} className="mb-4 p-2 bg-blue-500 text-white rounded">
-              {isFetching.current ? 'Stop' : 'Start'}
-            </button>
+                
+      <select value={fetchInterval} onChange={(e) => {
+        //console.log('fetchInterval onChange called with value:', e.target.value);
+        setFetchInterval(Number(e.target.value));
+      }} className="mb-4">
+        <option value={5}>5 seconds</option>
+        <option value={10}>10 seconds</option>
+        <option value={15}>15 seconds</option>
+        <option value={30}>30 seconds</option>
+        <option value={60}>60 seconds</option>
+      </select>
+      
+      <select value={chartType} onChange={(e) => {
+        //console.log('chartType onChange called with value:', e.target.value);
+        setChartType(e.target.value);
+      }} className="mb-4">
+        <option value="line">Line</option>
+        <option value="bar">Bar</option>
+      </select>
+          
+      <button onClick={toggleFetching} className="mb-4 p-2 bg-blue-500 text-white rounded">
+        {isFetching ? 'Stop' : 'Start'}
+      </button>
         </div>
       ) : (
         <h2>Please enter your API key and Channel ID in the sidebar to view data</h2>
